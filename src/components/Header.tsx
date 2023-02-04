3be595caa81a0d2d9981596f8661074376a7a380
Tsoppa <chogovadzetsotne@gmail.com>
2023-02-04 21:50:55 +0400

    set up environment

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+import {Box} from '@mui/material';
+
+const Header = () => {
+    return(
+        <Box>
+            123
+        </Box>
+    )
+}
+
+export default Header;
